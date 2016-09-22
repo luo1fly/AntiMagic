26f568df1c03d1b5153d87aa4d490cc994f26dc3
luo1fly <luo1fly@gmail.com>
2016-09-22 15:22:59 +0800

    添加redis保存token功能

--- assets/utils.py
+++ assets/utils.py
@@ -8,7 +8,10 @@ from hosts import models as hosts_models
 from django.shortcuts import render, HttpResponse
 from AntiMagic import settings
 from django.core.exceptions import ObjectDoesNotExist
+import redis
 # import custom modules above
+
+R = redis.Redis('192.168.0.195')
 
 
 def json_date_handler(obj):
@@ -23,11 +26,30 @@ def json_datetime_handler(obj):
 
 
 def gen_token(username, timestamp, token):
-    token_format = "%s\n%s\n%s" %(username,timestamp,token)
+    token_format = "%s\n%s\n%s" %(username, timestamp, token)
     obj = hashlib.md5()
     obj.update(token_format.encode('utf8'))
     # print '--->token format:[%s]'% token_format
     return obj.hexdigest()[10:17]
+
+
+def push_token_into_redis(token):
+    """
+    :param token: usertoken
+    :return:
+    """
+    R.set(token, 1, ex=settings.TOKEN_TIMEOUT)
+
+
+def token_exist_in_redis(token):
+    """
+    :param token: usertoken
+    :return:
+    """
+    if R.get(token):
+        return True
+    else:
+        return False
 
 
 def token_required(func):
@@ -45,14 +67,15 @@ def token_required(func):
             user_obj = hosts_models.UserProfile.objects.get(email=username)
             token_md5_from_server = gen_token(username, timestamp, user_obj.token)
             if token_md5_from_client != token_md5_from_server:
-                response['errors'].append({"auth_failed":"Invalid username or token_id"})
+                response['errors'].append({"auth_failed": "Invalid username or token_id"})
             else:
                 if abs(time.time() - int(timestamp)) > settings.TOKEN_TIMEOUT:
                     # default timeout 120
                     response['errors'].append({"auth_failed": "The token is expired!"})
                 else:
-                    pass
-                    # print "\033[31;1mPass authentication\033[0m"
+                    if token_exist_in_redis(token_md5_from_client):
+                        response['errors'].append({"auth_failed": "The token has been used!"})
+                        # redis里已有的key被认为已使用过
                 print(
                     "\033[41;1m;%s ---client:%s\033[0m" % (time.time(), timestamp),
                     time.time() - int(timestamp)
@@ -62,5 +85,7 @@ def token_required(func):
         if response['errors']:
             return HttpResponse(json.dumps(response))
         else:
+            push_token_into_redis(token_md5_from_client)
+            # 认证成功的token存到redis
             return func(*args, **kwargs)
     return wrapper
